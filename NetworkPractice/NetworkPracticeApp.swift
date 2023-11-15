64c7fac1548579f4322c6a2a78eadc2a334160e8
ciu <112779139+kangciu@users.noreply.github.com>
2023-11-15 14:02:50 +0900

    Initial Commit

---
+++ NetworkPractice/NetworkPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkPracticeApp.swift
+//  NetworkPractice
+//
+//  Created by 강치우 on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
